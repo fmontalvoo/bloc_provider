e220bf6f78ce319b52e0b03992cfb4bb30551f64
Frank Montalvo Ochoa <fmontalvoo@ivortec.com>
2020-09-20 11:48:22 -0500

    Implemento multi bloc provider.

--- lib/bloc_provider.dart
+++ lib/bloc_provider.dart
@@ -2,3 +2,4 @@ library bloc_provider;
 
 export 'src/bloc.dart';
 export 'src/bloc_provider.dart';
+export 'src/multi_bloc_provider.dart';
---
+++ lib/src/multi_bloc_provider.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+import 'package:bloc_provider/bloc_provider.dart';
+
+// STATEFULWIDGET
+
+class MultiBlocProvider<T extends Bloc> extends StatefulWidget {
+  final List<T> blocs;
+  final Widget child;
+
+  MultiBlocProvider({Key key, @required this.blocs, @required this.child})
+      : super(key: key);
+
+  static T of<T extends Bloc>(BuildContext context) =>
+      _MultiBlocProvider.of(context);
+
+  @override
+  State<StatefulWidget> createState() => _MultiBlocProviderState<T>();
+}
+
+// STATE
+
+class _MultiBlocProviderState<T extends Bloc>
+    extends State<MultiBlocProvider<T>> {
+  @override
+  Widget build(BuildContext context) {
+    return _MultiBlocProvider(
+      blocs: widget.blocs,
+      child: widget.child,
+    );
+  }
+
+  @override
+  void dispose() {
+    widget.blocs.forEach((bloc) => bloc.dispose());
+    super.dispose();
+  }
+}
+
+// INHERITEDWIDGET
+
+class _MultiBlocProvider<T extends Bloc> extends InheritedWidget {
+  final List<T> blocs;
+  final Widget child;
+
+  _MultiBlocProvider({Key key, @required this.blocs, @required this.child})
+      : super(key: key);
+
+  static T of<T extends Bloc>(BuildContext context) {
+    return context
+        .dependOnInheritedWidgetOfExactType<_MultiBlocProvider<Bloc>>()
+        .getBloc<T>();
+  }
+
+  Bloc getBloc<T extends Bloc>() {
+    for (int i = 0; i < blocs.length; i++)
+      if (blocs[i].runtimeType == T) return blocs[i];
+    throw FlutterError('Unable to find BLoC of type $T.');
+  }
+
+  @override
+  bool updateShouldNotify(_MultiBlocProvider oldWidget) {
+    return blocs != oldWidget.blocs;
+  }
+}
